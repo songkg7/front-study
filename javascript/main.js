3fcaad23edc751652e555726b1ce7fcff15499a5
Haril Song <songkg7@gmail.com>
2021-07-30 21:00:24 +0900

    js dom api 연습 3

--- javascript/main.js
+++ javascript/main.js
@@ -3,5 +3,13 @@ const boxEls = document.querySelectorAll('.box');
 boxEls.forEach((boxEl, index) => {
   boxEl.classList.add(`order-${index + 1}`);
   boxEl.textContent = `new content ${index}`;
-  console.log(index, boxEl);
+  // console.log(index, boxEl);
+});
+
+boxEls.forEach((boxEl, index) => {
+  console.log(boxEl.classList);
+  console.log(boxEl.className);
+  if (boxEl.classList.contains('active')) {
+    console.log(`here! ${index}`);
+  }
 });
