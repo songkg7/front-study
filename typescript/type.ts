d0598ff9ea14fe8bf03e327a1001cdc7d190036b
Haril Song <songkg7@gmail.com>
2021-08-03 00:38:18 +0900

    ts 연습 - type 추론

--- typescript/type.ts
+++ typescript/type.ts
@@ -14,11 +14,10 @@ console.log(f4(-5) + 5); // strictNullChecks
 
 // noImplicitReturns option
 // 반드시 모든 리턴의 경우를 구현해야한다.
-function f5(a: number) :number {
+function f5(a: number): number {
   if (a > 0) {
     return a * 10;
   } else {
     return -1;
   }
 }
-
