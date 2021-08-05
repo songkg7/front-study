955e2fc5c482ac2c990f5fd17a65221ccde77626
Haril Song <songkg7@gmail.com>
2021-08-05 10:52:24 +0900

    html plugins setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
 // const path = require('path')
+const HtmlPlugin = require("html-webpack-plugin");
 
 // export
 module.exports = {
   // 파일을 읽어들이기 시작하는 진입점 설정
-  entry: './js/main.js',
+  entry: "./js/main.js",
 
   // 결과물(번들)을 반환하는 설정
   output: {
     // path: path.resolve(__dirname, 'dist'),  // 절대경로를 제공해야 한다.
     // filename: 'main.js',
-    clean: true  // 기존 내용 제거
+    clean: true, // 기존 내용 제거
+  },
+
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+    }),
+  ],
+
+  devServer: {
+    host: 'localhost'
   }
-}
+};
